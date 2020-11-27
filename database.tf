799be663dea26a50c99c189213678983255c7d2a
Mohit Gupta <mo.gupta17@gmail.com>
2020-11-27 18:22:55 -0500

    Update database.tf

--- database.tf
+++ database.tf
@@ -52,7 +52,7 @@ resource "aws_rds_cluster" "aurora_db" {
 # AWS RDS Aurora Cluster Instances
 #------------------------------------------------------------------------------
 resource "aws_rds_cluster_instance" "aurora_db_cluster_instances" {
-  count                = length(var.availability_zones)
+  count                = 1
   identifier           = "aurora-db-instance-${count.index}"
   cluster_identifier   = aws_rds_cluster.aurora_db.id
   db_subnet_group_name = aws_db_subnet_group.aurora_db_subnet_group.id
